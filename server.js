110fb050f59800860cec4c25419acdf1cf9c1464
Nikhil Kaushik <nikhil.celtics@gmail.com>
2018-10-26 01:33:18 +0530

    creates server

---
+++ server.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const app = express();
+
+const config = require('./config');
+const port = config.port || 3000;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+let positions = {}; 
+
+// id|latx_longy_role,id|...
+app.post('/update', (req, res) => {
+	const positionString = req.body.payload;
+	const devices = positionString.split(",");
+	for (device in devices) {
+		const id = devices[device].split("|")[0];
+		const latx = devices[device].split("|")[1].split("_")[0];
+		const longy = devices[device].split("|")[1].split("_")[1];
+		const role = devices[device].split("|")[1].split("_")[2];
+		positions[id] = { latx, longy, role };
+	};
+	res.send(generateString());
+});
+
+generateString = () => {
+	let output = "";
+	for (id in positions) {
+		output += id + "|" + positions[id].latx + "_" + positions[id].longy + "_" + positions[id].role + ",";
+	}
+	return output;
+}
+
+app.listen(port, () => console.log("Listening at port " + port));
